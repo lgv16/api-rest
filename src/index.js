bab2e8111500c28e08fb22ecf3f6b65bb2883708
Laryan <laryangv@gmail.com>
2020-08-15 11:32:17 -0400

    commits edit

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan")
 
-//settings
-app.set('port', process.env.PORT || 3000);// process.env.PORT es para el caso de que exista algun puerto definido por algun servicio del sistema o nube que o use en el caso contrario, solo usar el puerto 3000
+//configuración
+app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
 
 //middlewares
 app.use(morgan('dev'));//para visualizar por consola las peticiones
-app.use(express.urlencoded({extended: false}));//tratar de entender los datos que vienen de formularios, como texto
-app.use(express.json());//para recibir formatos json y entenderlos
+app.use(express.urlencoded({extended: false}));//tratar de entender los datos que vienen de formularios, como texto.
+app.use(express.json());//para recibir formatos JSON
 
 //routes 
-app.use('/api/lists',require('./routes/lists')); //con api/todolist estamos sentenciando que todas las rutas comenzaran con esa ruta
+app.use('/api/lists',require('./routes/lists')); //con "api/lists" sentenciamos que todas las rutas comenzaran con esa ruta, por buena practica de apis
 
-// para iniciar el servidor en el puerto 3000
+//iniciamos el servidor en el puerto 3000
 app.listen(app.get('port'), () => {
  console.log(`El servidor está inicializado en el puerto ${app.get('port')}`);
 });
--- src/routes/lists.js
+++ src/routes/lists.js
@@ -1,67 +1,67 @@
-const { Router } = require('express');//desde express requiero su metodo Router
-const router = Router();//ejecutar Router en una constante llamada router, permitiendome crear nuevas rutas
+const { Router } = require('express');
+const router = Router();
 const _ = require('underscore');
 
-const lists = require('../sample.json'); //simulación de una DB
+const lists = require('../sample.json'); //archivo json que simula una DB
 console.log(lists);
 
-router.get('/', (req, res) => {//al recibir algo a esta ruta, enviare algo
-    res.json(lists);//aquí podemos enviar objetos tales como los archivos JSON
+router.get('/', (req, res) => {//Petición GET. Al recibir algo a esta ruta, se enviará una respuesta
+    res.json(lists);//aquí podemos enviar objetos tales como el archivo JSON
 });
 
-router.post('/', (req, res) => {
-    const {nombre, estado, descripcion } = req.body;//desde request body quiero obtener el nombre, estado y descripcion de la lista.
+router.post('/', (req, res) => {//Petición POST. Desde el request body quiero obtener el nombre, estado o descripcion de la lista.
+    const {nombre, estado, descripcion } = req.body;
     if (nombre && estado && descripcion) { //validación de que estamos recibiendo todos estos datos.
         const id = lists.length + 1; //como id es un identificador unico y que debe generarse solo, lo definimos aqui
         const newList = {...req.body, id}; //pasamos todo el objeto del request body en un nuevo objeto
         console.log(newList);
-        lists.push(newList);//almacenamos
-        res.json(lists);//aquí podemos enviar objetos tales como los archivos JSON
+        lists.push(newList);//almacenamos en memoria el nuevo objeto
+        res.json(lists);
     } else {
-        res.send('Petición errónea');
+        res.send('Petición errónea');//en caso de error en la petición.
     }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res) => {//Función PUT.
     const { id } = req.params;
-    const {nombre, estado, descripcion } = req.body;//desde request body quiero obtener el nombre, estado y descripcion de la lista.
+    const {nombre, estado, descripcion } = req.body;
     
-    if (nombre) { //validación de que estamos recibiendo todos estos datos.
+    if (nombre) { //validación de que estamos recibiendo este dato.
         _.each(lists, (list, i) => {
             if (list.id == id) {
-                list.nombre = nombre;
+                list.nombre = nombre;//asignamos el nuevo dato
             }
         });
     }
     
-    if (estado) {
+    if (estado) {//validación de que estamos recibiendo este dato.
         _.each(lists, (list, i) => {
             if (list.id == id) {
-                list.estado = estado;
+                list.estado = estado;//asignamos el nuevo dato
             }
         });
     }
 
-    if (descripcion) {
+    if (descripcion) {//validación de que estamos recibiendo este dato.
         _.each(lists, (list, i) => {
             if (list.id == id) {
-                list.descripcion = descripcion;
+                list.descripcion = descripcion;//asignamos el nuevo dato
             }
         });
     }
 
-    res.json(lists);//aquí podemos enviar objetos tales como los archivos JSON
+    res.json(lists);//enviamos la respuesta de objetos JSON
 
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {//Funcion DELETE.
     const { id } = req.params;
     _.each(lists, (list, i) => {//empieza a recorrer todo el arreglo de lists, del cual se obtiene una funcion que recibe una lista y un indice.
         if (list.id == id) {//si se cumple esta sentencia, removemos el id solicitado
-            lists.splice(i, 1);//aqui se remueve
+            lists.splice(i, 1);//aqui se remueve con la funcion SPLICE
         }
     });
-    res.json(lists);//aquí podemos enviar objetos tales como los archivos JSON    
+    res.json(lists);
 });
 
 module.exports = router;//exportamos al index.js principal
